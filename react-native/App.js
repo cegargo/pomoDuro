eeb4b658e0235ca8ff4ac60ac9ca784851bcf6d2
cegargo <clintegargo@protonmail.ch>
2022-11-20 10:49:12 +0800

    Added: pomoDuro (React Native)

---
+++ react-native/App.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) cegargo
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
